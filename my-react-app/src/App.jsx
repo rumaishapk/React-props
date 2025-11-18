d2016cd89adef1c395648bc70bcc8a3ee8191e4f
rumaishapk <rumaisharumi27@gmail.com>
2025-11-18 12:43:58 +0530

    emojipedia completed

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,28 +1,28 @@
-import Card from "./Card";
+import React from "react";
+import emojipedia from "./emojipedia";
 
 function App() {
   return (
     <div>
-      <h1 className = "heading">My contacts</h1>
-      <Card
-        name="Dulqar"
-        image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/dulquer-salmaan-37626-19-09-2017-04-04-09.jpg"
-        phoneno="9947257480"
-        gmail="dulqar123@gmail.com"
-      />
-      <Card
-        name="mammootty"
-        className="circle-img"
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS2WV9IQNxjwlk0kLTXOujLHBirJkTED7gGXg&s"
-        phoneno="9072203040"
-        gmail="mamotty321@gmail.com"
-      />
-      <Card
-        name="Nazriya"
-        image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXmIpYJG8wt4ZQDzgTT__RwANpyr1VOB5Cew&s"
-        phoneno="987654321"
-        gmail="nazri1@gmail.com"
-      />
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">
+        {emojipedia.map((emoji, index) => (
+          <div className="term">
+            <dt>
+              <span className="emoji" role="img" aria-label="Tense Biceps">
+                {emoji.emoji}
+              </span>
+              <span >{emoji.name}</span>
+            </dt>
+            <dd>
+              {emoji.meaning}
+            </dd>
+          </div>
+        ))}
+      </dl>
     </div>
   );
 }
---
+++ my-react-app/src/emojipedia.js
@@ -0,0 +1,45 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "🙂",
+    name: "Slightly Smiling Face",
+    meaning:
+      "This slightly smiling emoji expresses a pleasant contentment and inner peace. It is used to make a statement seem friendlier, even if it is not necessarily meant that way."
+  },
+  {
+    id: 5,
+    emoji: "🥵",
+    name: "Overheated face",
+    meaning:
+      "It is so hot, I will get a heat stroke right away! Bright red head, the tongue is hanging from the mouth and drops of sweat are on the forehead. The hottest place in the world lies in Iran: In 2005, the highest temperature of 70.7 °C was measured in the Dasht-e Lut desert."
+  },
+  {
+    id: 6,
+    emoji: "🤗",
+    name: "Hugging Face",
+    meaning:
+      "Smiley with red cheeks is stretching out its hands toward you. Wants to hug you. It's an open and cordial gesture and an expression of warmth and friendliness."
+  }
+];
+ export default emojipedia
