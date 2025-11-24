cdf7a9154faa252a7e7035fb6a5a47a9cfb3d7f6
rumaishapk <rumaisharumi27@gmail.com>
2025-11-24 12:38:32 +0530

    counter practice

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import Form from "./Form";
-
-var userIsRegistered =false;
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const handleclick = () => setCount(count + 1);
+  const handleclick2 = () => {
+    count == 0 ? null : setCount(count - 1);
+  };
+
   return (
-    <div className="container">
-      <Form isRegistered={userIsRegistered} />
+    <div className="box">
+      counter
+      <br />
+      {count}
+      <br />
+      <button onClick={handleclick}>+</button>
+      <button onClick={handleclick2}>-</button>
     </div>
   );
 }
